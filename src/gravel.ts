13c3500229e984badc7ea7472f8e0b550da9a401
Sebastian Sebald <sebastian.sebald@gmail.com>
2015-05-18 13:36:58 +0200

    chore(*): Setup gravel.ts files.

---
+++ src/gravel.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/angular2/angular2" />
+
+import {bootstrap, Component, View} from "angular2/angular2";
+
+@Component({
+	selector: 'gravel-app'
+})
+@View({
+	template: `Hello there!!!`
+})
+class GravelApp {}
