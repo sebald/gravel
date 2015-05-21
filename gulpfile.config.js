e17e7148c6ef199320ded2e859ed3f1c215c37f4
Sebastian Sebald <sebastian.sebald@gmail.com>
2015-05-21 13:30:58 +0200

    chore(gulp): Add tasks to copy libs and mains files.

---
+++ gulpfile.config.js
@@ -0,0 +1,41 @@
+'use strict';
+
+function Config () {	
+	var config = this;
+
+	// Paths
+	config.path = {
+		src: './src',
+		dest: './build',
+		
+		typings: './typings'
+	};
+	
+	
+	// Third Party Libraries
+	config.libs = [
+		'node_modules/es6-promise/dist/es6-promise.js',
+		'node_modules/whatwg-fetch/fetch.js'		
+	];
+	
+	
+	// Typescript
+	config.typescript = {
+		files: config.path.src + '/**/*.ts',
+		tsd: config.path.typings + '/**/d.ts',
+		tsconfig: {
+			"emitDecoratorMetadata": true,
+			"module": "commonjs",
+			"target": "es5",
+			"removeComments": true		
+		}		
+	};
+	
+	
+	// Main (index.html)
+	config.main = config.path.src + '/index.html';	
+}
+
+module.exports = function () {
+	return new Config();	
+};
--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,45 @@ var gulp = require('gulp'),
 	ts = require('gulp-typescript'),
 	sourcemaps = require('gulp-sourcemaps'),	
 	
-	browserfiy = require('browserify'),
+	del = require('del'),
 	assign = require('object-assign'),
 	
-	
-	tsconfig = require('./tsconfig');
+
+	// Load configuration
+	config = require('./gulpfile.config.js')();
+
+
+gulp.task('clean', function (done) {
+	del([config.path.dest], done);
+});
 
 
 gulp.task('source', function () {
-	var files = tsconfig.filesGlob,
+	var files = config.typescript.files,
 		tsResult;
-		
+
 	tsResult = gulp.src(files)
 		.pipe(sourcemaps.init())
 		.pipe(ts(assign(
-			{ typescript: require('typescript') },
-			tsconfig.compilerOptions,
+			{ typescript: require('typescript') }, // Need to load v1.5-beta
+			config.typescript.tsconfig,
 			{ sortOutput: true }
 		)));
 		
 	return tsResult.js
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest(config.path.dest));
+});
+
+
+gulp.task('libs', function () {
+	gulp.src(config.libs)
+		.pipe(gulp.dest(config.path.dest + '/libs'));
+});
+
+
+gulp.task('main', function () {
+	console.log(config.main);
+	gulp.src(config.main)
+		.pipe(gulp.dest(config.path.dest));
 });
